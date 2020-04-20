a5ed12ff0250e79b48d71f1ad4acfac092adb473
Ennie <ennieyusuf@gmail.com>
2020-04-20 04:49:23 +0000

    Bootstrap Project

---
+++ bootstrap/javascript.js
@@ -0,0 +1,22 @@
+    // // // alert('hello');
+
+
+    // // function clickMe(){
+    // //     alert('Login');
+    // // }
+
+
+    // function moveOver(){
+    //     alert('hey there');
+    // }
+
+    // function change(){
+    //     document.getElementById('head').style.display='none';
+    // }
+
+
+    var hello=[
+        'ama','23','kwaku'
+    ];
+    alert(hello.length);
+    //var dont use quotations only strings
